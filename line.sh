4366bce62d3573b48bcb2779c1f7dcc91e364040
HIRAO Tetsuya <hirao@radio.sarami.info>
2022-01-02 09:09:49 +0900

    renewal

--- line.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ACCESS_TOKEN=22a5rDpEZa1qp3Ckrr1CZErRkGHgUM2ZXX7NNgcQYnr
-MSG=$1
-
-curl -s -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -F "message=$MSG" https://notify-api.line.me/api/notify >/dev/null
